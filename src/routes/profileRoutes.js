f30828ec42793ec04d8b86a5e77929729f87a501
Asim Shakoor <asimshakoor920830@gmail.com>
2024-07-25 19:00:12 +0500

    profile

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,23 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const Profile = require('../models/Profile');
-const path = require('path');
 const jwt = require('jsonwebtoken');
 const verifyToken = require('../middleware/authMiddleware'); // Import your JWT middleware
 
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
 // Configure Multer storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // cb(null, 'uploads/');
-    cb(null, '/tmp/uploads/');
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     // cb(null, 'uploads/');
+//     cb(null, '/tmp/uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   }
+// });
+
+// const upload = multer({ storage: storage });
+
+
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Cloudinary storage
+const cloudinaryStorage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads',
+    format: async (req, file) => 'png', // supports promises as well
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`,
   },
-  filename: function (req, file, cb) {
+});
+
+// Local storage
+const localStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
   }
 });
 
-const upload = multer({ storage: storage });
+// Choose storage based on environment
+const storage = process.env.NODE_ENV === 'production' ? cloudinaryStorage : localStorage;
+const upload = multer({ storage });
 
 // Middleware to verify JWT token
 router.use(verifyToken);
@@ -37,8 +74,8 @@ router.get('/:username', async (req, res) => {
 router.post('/:username', upload.single('photo'), async (req, res) => {
   const { file, body } = req;
   if (file) {
-    // body.photo = `/uploads/${file.filename}`;
-    body.photo = `/tmp/uploads/${file.filename}`;
+    // body.photo = `/uploads/${file.filename}`; 
+    body.photo = process.env.NODE_ENV === 'production' ? file.path : `/uploads/${file.filename}`;   
   }
 
   try {
